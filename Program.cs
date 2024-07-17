3e49f3cd4fca412fa5400dc2700c5a4bd3fd6814
Saiel Shinde <69382578+saielshinde@users.noreply.github.com>
2024-07-17 21:59:11 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Scheduler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.StreamWriter OurStream;
+            OurStream = File.CreateText(@"C:\temp\text.txt");
+            OurStream.WriteLine("This text is for task scheduler demo test file created on at : " + DateTime.Now);
+            OurStream.Close();
+            Console.WriteLine("File Created successfully!");
+
+            string logFilePath = @"C:\temp\log.txt";
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(logFilePath, true))
+                {
+                    writer.WriteLine("Logging at " + DateTime.Now);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error occurred while logging: {ex.Message}");
+            }
+        }
+    }
+}
